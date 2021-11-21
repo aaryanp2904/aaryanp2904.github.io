d7ae2689f53181cdc8a1cc78ca65e3a4a8bedf67
aaryanp2904 <64792565+aaryanp2904@users.noreply.github.com>
2021-11-21 17:20:50 +0000

    Update host2.js

--- public/js/host2.js
+++ public/js/host2.js
@@ -69,7 +69,7 @@ startGamebtn.addEventListener('click', function () {
     console.log('ready')
 
     // Hide the start game button
-    startGamebtn.style.display = 'none'
+    delete startGamebtn;
 })
 
 
